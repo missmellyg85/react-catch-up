10193b8d35fc1dede6d030e8d86c74877670c38a
Missy Williams <missywilliams@carfax.com>
2020-11-19 15:32:14 -0600

    Now we have a winner. Seems straightforward enough

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = [...this.state.squares];
+    if (calculateWinner(squares) || squares[i]) return;
     squares[i] = this.state.xIsNext ? "X" : "O";
     this.setState({ squares, xIsNext: !this.state.xIsNext });
   }
@@ -37,7 +38,11 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = `Next player: ${this.state.xIsNext ? "X" : "O"}`;
+    const winner = calculateWinner(this.state.squares);
+
+    const status = winner
+      ? `The winner is ${winner}!!`
+      : `Next player: ${this.state.xIsNext ? "X" : "O"}`;
 
     return (
       <div>
@@ -76,6 +81,26 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================
